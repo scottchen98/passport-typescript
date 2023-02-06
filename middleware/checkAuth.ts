f963767bcfdbf13a91ba9b49d4e863a531ac12f2
Scott Chen <scott_chen@Scotts-MacBook-Pro.local>
2023-02-06 12:26:52 -0800

    fix checkAuth typeerrors

--- middleware/checkAuth.ts
+++ middleware/checkAuth.ts
@@ -1,19 +1,24 @@
+import { Request, Response, NextFunction } from "express";
 /*
 FIX ME (types) 😭
 */
-export const ensureAuthenticated = (req: any, res: any, next: any) => {
+export const ensureAuthenticated = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   if (req.isAuthenticated()) {
     return next();
   }
   res.redirect("/auth/login");
-}
+};
 
 /*
 FIX ME (types) 😭
 */
 export const forwardAuthenticated = (req: any, res: any, next: any) => {
-    if (!req.isAuthenticated()) {
-      return next();
-    }
-    res.redirect("/dashboard");
-}
+  if (!req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/dashboard");
+};
