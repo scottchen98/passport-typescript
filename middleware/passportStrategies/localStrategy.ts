7f2e9398d415d7b49a72796a97c39cce11db663f
Scott Chen <scott_chen@Scotts-MacBook-Pro.local>
2023-02-06 16:43:41 -0800

    fix localStrategy

--- middleware/passportStrategies/localStrategy.ts
+++ middleware/passportStrategies/localStrategy.ts
@@ -1,7 +1,10 @@
 import passport from "passport";
 import { Strategy as LocalStrategy } from "passport-local";
-import { getUserByEmailIdAndPassword, getUserById} from "../../controllers/userController";
-import { PassportStrategy } from '../../interfaces/index';
+import {
+  getUserByEmailIdAndPassword,
+  getUserById,
+} from "../../controllers/userController";
+import { PassportStrategy } from "../../interfaces/index";
 
 const localStrategy = new LocalStrategy(
   {
@@ -21,14 +24,20 @@ const localStrategy = new LocalStrategy(
 /*
 FIX ME (types) 😭
 */
-passport.serializeUser(function (user: any, done: any) {
+passport.serializeUser(function (
+  user: Express.User,
+  done: (err: any, id: number) => void
+) {
   done(null, user.id);
 });
 
 /*
 FIX ME (types) 😭
 */
-passport.deserializeUser(function (id: any, done: any) {
+passport.deserializeUser(function (
+  id: number,
+  done: (err: any, user?: Express.User | false | null) => void
+) {
   let user = getUserById(id);
   if (user) {
     done(null, user);
@@ -38,7 +47,7 @@ passport.deserializeUser(function (id: any, done: any) {
 });
 
 const passportLocalStrategy: PassportStrategy = {
-  name: 'local',
+  name: "local",
   strategy: localStrategy,
 };
 
