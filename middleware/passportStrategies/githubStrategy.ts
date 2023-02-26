bdf39442ff5417115d88f97b421f0dc00a36cf91
Scott Chen <scott_chen@Scotts-MacBook-Pro.local>
2023-02-26 13:18:12 -0800

    fix profile type

--- middleware/passportStrategies/githubStrategy.ts
+++ middleware/passportStrategies/githubStrategy.ts
@@ -3,6 +3,51 @@ import { PassportStrategy } from "../../interfaces/index";
 import { findOrCreateUser } from "../../controllers/userController";
 require("dotenv").config();
 
+interface GitHubProfile {
+  id: string;
+  nodeId: string;
+  displayName: string;
+  username: string;
+  profileUrl: string;
+  photos: { value: string }[];
+  provider: string;
+  _raw: JSON;
+  _json: {
+    login: string;
+    id: number;
+    node_id: string;
+    avatar_url: string;
+    gravatar_id: string;
+    url: string;
+    html_url: string;
+    followers_url: string;
+    following_url: string;
+    gists_url: string;
+    starred_url: string;
+    subscriptions_url: string;
+    organizations_url: string;
+    repos_url: string;
+    events_url: string;
+    received_events_url: string;
+    type: string;
+    site_admin: boolean;
+    name: string;
+    company: string | null;
+    blog: string;
+    location: string;
+    email: string | null;
+    hireable: string | null;
+    bio: string | null;
+    twitter_username: string | null;
+    public_repos: number;
+    public_gists: number;
+    followers: number;
+    following: number;
+    created_at: string;
+    updated_at: string;
+  };
+}
+
 const githubStrategy: GitHubStrategy = new GitHubStrategy(
   {
     clientID: process.env.CLIENT_ID ? process.env.CLIENT_ID : "",
@@ -16,10 +61,10 @@ const githubStrategy: GitHubStrategy = new GitHubStrategy(
     req: Express.Request,
     accessToken: string,
     refreshToken: string,
-    profile: any,
+    profile: GitHubProfile,
     done: (err?: Error | null, user?: Express.User, info?: object) => void
   ): Promise<void> => {
-    const user = findOrCreateUser(profile.id, profile.displayName);
+    const user = findOrCreateUser(Number(profile.id), profile.displayName);
     done(null, user);
   }
 );
